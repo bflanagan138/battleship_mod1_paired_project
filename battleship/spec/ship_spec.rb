a6f52137f544909404a269b29fa7bb1c8456616c
Kyle Ledin <Kyle.Ledin85@gmail.com>
2022-10-17 18:43:26 -0500

    feature: Finish ship class for IT1

--- battleship/spec/ship_spec.rb
+++ battleship/spec/ship_spec.rb
@@ -2,6 +2,7 @@ require 'rspec'
 require './lib/ship'
 
 RSpec.describe Ship do
+
   it 'exists and has attributes' do
     cruiser = Ship.new("Cruiser", 3)
 
@@ -9,4 +10,54 @@ RSpec.describe Ship do
     expect(cruiser.name).to eq("Cruiser")
     expect(cruiser.length).to eq(3)
   end
+
+  it 'has health' do
+    cruiser = Ship.new("Cruiser", 3)
+
+    expect(cruiser.health).to eq(3)
+  end
+
+  it 'loses health when hit' do
+    cruiser = Ship.new("Cruiser", 3)
+    cruiser.hit
+
+    expect(cruiser.health).to eq(2)
+
+    cruiser.hit
+
+    expect(cruiser.health).to eq(1)
+
+  end
+
+  it 'it is not sunk' do
+    cruiser = Ship.new("Cruiser", 3)
+
+    expect(cruiser.sunk?).to eq(false)
+
+    cruiser.hit
+
+    expect(cruiser.sunk?).to eq(false)
+
+    cruiser.hit
+
+    expect(cruiser.sunk?).to eq(false)
+  end
+
+  it 'it will sink' do
+    cruiser = Ship.new("Cruiser", 3)
+
+    expect(cruiser.sunk?).to eq(false)
+
+    cruiser.hit
+
+    expect(cruiser.sunk?).to eq(false)
+
+    cruiser.hit
+
+    expect(cruiser.sunk?).to eq(false)
+
+    cruiser.hit
+
+    expect(cruiser.sunk?).to eq(true)
+    end
 end
