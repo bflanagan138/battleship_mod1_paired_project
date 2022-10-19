8adcced29fa488bed0440ba1559b761436607d3e
Bryan Flanagan <bflanagan78@yahoo.com>
2022-10-18 18:33:43 -0600

    feat: Add cell_spec

--- battleship/spec/cell_spec.rb
+++ battleship/spec/cell_spec.rb
@@ -25,4 +25,13 @@ RSpec.describe Cell do
 
     expect(cell.empty?).to eq (true)
   end
+
+  it 'has a ship' do
+    cell = Cell.new('B4')
+    cruiser = Ship.new("Cruiser", 3)
+    cell.place_ship(cruiser)
+
+    expect(cell.ship).to eq (cruiser)
+    expect(cell.empty?).to eq (false)
+  end
 end
