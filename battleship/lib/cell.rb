683d834c995087c8c4bde77a2ad70f97d6c97451
Bryan Flanagan <bflanagan78@yahoo.com>
2022-10-18 18:33:12 -0600

    feat: Add place ship method

--- battleship/lib/cell.rb
+++ battleship/lib/cell.rb
@@ -1,18 +1,24 @@
 class Cell
-  attr_reader :coordinate
+  attr_reader :coordinate,
+              :ship
 
   def initialize(coordinate)
     @coordinate = coordinate
+    @ship = ship
   end
 
-  def ship
-  end
-  
   def empty?
     if @ship == nil
       true
     else
       false
+    end
+  end
+# place ship will change empty? from true to false
+  def place_ship(place)
+    # require 'pry'; binding.pry
+    if empty? == true
+      @ship = place
     end
   end
 end
