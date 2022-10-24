ad68bee103da8872b25e748be6364c9c4fcecb1e
Bryan Flanagan <bflanagan78@yahoo.com>
2022-10-24 10:41:20 -0600

    WIP: add pseudo code to describe turn

--- battleship/lib/setup.rb
+++ battleship/lib/setup.rb
@@ -4,31 +4,9 @@ require './lib/ship'
 require './lib/turn'
 
 class Setup
-  # attr_reader :computer
+
   def initialize
   end
-
-  # def computer
-  #   board = Board.new
-  #   cruiser = Ship.new(cruiser, 3)
-  #   submarine = Ship.new(submarine, 2)
-  #     def place_cruiser
-  #       computer_cruiser = board.cells.keys.shuffle.slice(0..2)
-  #       while board.valid_placement?(cruiser, computer_cruiser) == false
-  #         computer_cruiser = board.cells.keys.shuffle.slice(0..2)
-  #       end
-  #       board.place(cruiser, computer_cruiser)
-  #     end
-  #     def place_submarine
-  #       computer_submarine = board.cells.keys.shuffle.slice(0..1)
-  #       while board.valid_placement?(submarine, computer_submarine) == false
-  #         computer_submarine = board.cells.keys.shuffle.slice(0..1)
-  #       end
-  #       board.place(submarine, computer_submarine)
-  #       board.render
-  #     end
-  #
-  # end
 
   def start_game
     #computer
@@ -36,17 +14,16 @@ class Setup
     cruiser = Ship.new(cruiser, 3)
     submarine = Ship.new(submarine, 2)
     computer_board = Board.new
+    turn = Turn.new
     computer_cruiser = Ship.new(computer_cruiser, 3)
     computer_submarine = Ship.new(computer_submarine, 2)
 
-    # def place_cruiser
       computer_cruiser_cells = computer_board.cells.keys.shuffle.slice(0..2)
       while computer_board.valid_placement?(computer_cruiser, computer_cruiser_cells) == false
         computer_cruiser_cells = computer_board.cells.keys.shuffle.slice(0..2)
       end
       computer_board.place(computer_cruiser, computer_cruiser_cells)
-    # end
-    # def place_submarine
+   
       computer_submarine_cells = computer_board.cells.keys.shuffle.slice(0..1)
       while computer_board.valid_placement?(computer_submarine, computer_submarine_cells) == false
         computer_submarine_cells = computer_board.cells.keys.shuffle.slice(0..1)
@@ -75,16 +52,13 @@ class Setup
     end
     board.place(submarine, squares_submarine)
 
-    #def turn
       puts "=============COMPUTER BOARD============="
-      #require 'pry'; binding.pry
       puts computer_board.render
       puts "==============PLAYER BOARD=============="
       puts board.render(true)
-    #end
-    #turn
+      puts ""
+      puts "Enter the coordinate for your shot:"
+      guess = gets.chomp.upcase
+    turn
   end
-
 end
-# end
-# turn
--- battleship/lib/turn.rb
+++ battleship/lib/turn.rb
@@ -2,14 +2,33 @@ require './lib/board'
 require './lib/cell'
 require './lib/setup'
 
-#display boards
-# Player choosing a coordinate to fire on
-# Computer choosing a coordinate to fire on
-# Reporting the result of the Player’s shot
-# Reporting the result of the Computer’s shot
-# game = Setup.new
-def turn
-  puts "=============COMPUTER BOARD=============" 
-  require 'pry'; binding.pry
-  puts "==============PLAYER BOARD==============" 
+class Turn
+#display game boards
+#take a guess against computer
+#display feedback??? 
+#check for win/game over???
+#iteration pattern indicates we don't display feedback or check for game over until after both player and computer take turn
+#what if player wins and computer wins?
+#computer plays their turn (behind the scenes)
+#display feedback
+#check for win/game over
+#if game over, send to gameover.rb
+##else start new turn
+  def initialize(guess)
+    @guess = guess
+  end
+
+  def turn
+    puts "=============COMPUTER BOARD============="
+      #require 'pry'; binding.pry
+      puts computer_board.render
+      puts "==============PLAYER BOARD=============="
+      puts board.render(true)
+  end
+
+  def correct?
+    #check if guess is valid
+    ##invalid for guesses if already guessed that cell
+    #check if guess is a miss, hit, hit & sunk
+  end
 end
